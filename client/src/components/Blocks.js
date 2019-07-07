332de8f0a0714ee288c97936dfe407ea17eb9fa5
Roshan Sureen <rsureen.tech@gmail.com>
2019-07-07 17:57:53 +0200

    fetching genesis blocks

---
+++ client/src/components/Blocks.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Blocks extends Component {
+  state = { blocks: [] };
+  componentDidMount() {
+    fetch("http://localhost:3000/api/blocks")
+      .then(response => response.json())
+      .then(json => this.setState({ blocks: json }))
+      .catch(err => console.log(err));
+  }
+  render() {
+    console.log("this.state", this.state);
+    return (
+      <div>
+        <h3>Blocks</h3>
+        {this.state.blocks.map(block => {
+          return <div key={block.hash}>{block.hash}</div>;
+        })}
+      </div>
+    );
+  }
+}
