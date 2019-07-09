1f4e9507d649188b0cb2b80ddebfc380f41e25f5
Roshan Sureen <rsureen.tech@gmail.com>
2019-07-09 23:55:23 +0200

    seed the backend with data

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -25959,7 +25959,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50748" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52043" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
